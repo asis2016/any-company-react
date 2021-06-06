99ef8359e89b25da3dd65085e7846af9bd77e83a
Ashish <mhrj.asis@gmail.com>
2021-06-06 11:26:22 +0200

    add contact

--- src/App.tsx
+++ src/App.tsx
@@ -394,6 +394,33 @@ const App = (): JSX.Element => {
             </a>
         </div>
 
+        {/* contact-clean */}
+        <section className="contact-clean">
+            <form method="post">
+                <h2 className="text-center">Contact us</h2>
+                <div className="mb-3">
+                    <input className="form-control" type="text" name="name" placeholder="Name"/>
+                </div>
+                <div className="mb-3">
+                    <input className="form-control is-invalid"
+                           type="email"
+                           name="email"
+                           placeholder="Email"/>
+                    <small className="form-text text-danger">
+                        Please enter a
+                        correct email address.
+                    </small>
+                </div>
+                <div className="mb-3">
+                    <textarea className="form-control"
+                              name="message"
+                              placeholder="Message"></textarea>
+                </div>
+                <div className="mb-3">
+                    <button className="btn btn-primary" type="submit">send</button>
+                </div>
+            </form>
+        </section>
 
 
     </AppBase>
