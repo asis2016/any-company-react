30e8cc94da7294fc77be604c40b55102b152c76a
Ashish <mhrj.asis@gmail.com>
2021-06-06 11:19:51 +0200

    add testimonials-clean

--- src/App.tsx
+++ src/App.tsx
@@ -287,12 +287,99 @@ const App = (): JSX.Element => {
                     <Col md={4} sm={4}>
                         <div className="d-none d-md-block phone-mockup">
                             <img className="device"
-                                 src="https://amaharjan.com/projects/any-company/assets/images/phone.svg" />
+                                 src="https://amaharjan.com/projects/any-company/assets/images/phone.svg"/>
                             <div className="screen"></div>
                         </div>
                     </Col>
                 </Row>
             </Container>
+        </section>
+
+        {/* photo-gallery */}
+        <section className="photo-gallery">
+            <div className="container">
+                <div className="intro">
+                    <h2 className="text-center">Lightbox Gallery</h2>
+                    <p className="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum
+                        nibh erat, pellentesque ut laoreet vitae. </p>
+                </div>
+                <div className="row photos" data-bss-baguettebox="">
+                    <div className="col-sm-6 col-md-4 col-lg-3 item"><a
+                        href="https://amaharjan.com/projects/any-company/assets/images/desk.jpg"><img
+                        className="img-fluid" src="https://amaharjan.com/projects/any-company/assets/images/desk.jpg"/></a>
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 item"><a
+                        href="https://amaharjan.com/projects/any-company/assets/images/building.jpg"><img
+                        className="img-fluid"
+                        src="https://amaharjan.com/projects/any-company/assets/images/building.jpg"/></a></div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 item"><a
+                        href="https://amaharjan.com/projects/any-company/assets/images/loft.jpg"><img
+                        className="img-fluid" src="https://amaharjan.com/projects/any-company/assets/images/loft.jpg"/></a>
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 item"><a
+                        href="https://amaharjan.com/projects/any-company/assets/images/building.jpg"><img
+                        className="img-fluid"
+                        src="https://amaharjan.com/projects/any-company/assets/images/building.jpg"/></a></div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 item"><a
+                        href="https://amaharjan.com/projects/any-company/assets/images/loft.jpg"><img
+                        className="img-fluid" src="https://amaharjan.com/projects/any-company/assets/images/loft.jpg"/></a>
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 item"><a
+                        href="https://amaharjan.com/projects/any-company/assets/images/desk.jpg"><img
+                        className="img-fluid" src="https://amaharjan.com/projects/any-company/assets/images/desk.jpg"/></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {/* testimonial-clean */}
+        <section className="testimonials-clean">
+            <div className="container">
+                <div className="intro">
+                    <h2 className="text-center">Testimonials </h2>
+                    <p className="text-center">Our customers love us! Read what they have to say below. Aliquam sed
+                        justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae.
+                    </p>
+                </div>
+                <div className="row people">
+                    <div className="col-md-6 col-lg-4 item">
+                        <div className="box">
+                            <p className="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus.
+                                Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+                        </div>
+                        <div className="author">
+                            <img className="rounded-circle"
+                                 src="https://amaharjan.com/projects/any-company/assets/images/1.jpg"/>
+                            <h5 className="name">Ben Johnson</h5>
+                            <p className="title">CEO of Company Inc.</p>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4 item">
+                        <div className="box">
+                            <p className="description">Praesent aliquam in tellus eu gravida. Aliquam varius finibus
+                                est, et interdum justo suscipit id.</p>
+                        </div>
+                        <div className="author">
+                            <img className="rounded-circle"
+                                 src="https://amaharjan.com/projects/any-company/assets/images/3.jpg"/>
+                            <h5 className="name">Carl Kent</h5>
+                            <p className="title">Founder of Style Co.</p>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-lg-4 item">
+                        <div className="box">
+                            <p className="description">Aliquam varius finibus est, et interdum justo suscipit. Vulputate
+                                quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p>
+                        </div>
+                        <div className="author">
+                            <img className="rounded-circle"
+                                 src="https://amaharjan.com/projects/any-company/assets/images/2.jpg"/>
+                            <h5 className="name">Emily Clark</h5>
+                            <p className="title">Owner of Creative Ltd.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
 
